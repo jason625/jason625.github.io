c8437bcd6957c82e2d5c9f2334a6c3393a84a827
awjae <djwotmd@gmail.com>
2021-12-22 19:23:21 +0900

    도가토 업데이트

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ function add() {
         <td><input type="checkbox" id="c${cnt}-todo_6"/><label for="c${cnt}-todo_6"></label></td>
         <td class="${ parseInt(lv) < 1475 ? 'high' : ''}"><input type="checkbox" id="c${cnt}-todo_7"/><label for="c${cnt}-todo_7"></label></td>
         <td class="${ parseInt(lv) < 1490 ? 'high' : ''}"><input type="checkbox" id="c${cnt}-todo_8"/><label for="c${cnt}-todo_8"></label></td>
-        <td><input type="checkbox" id="c${cnt}-todo_9"/><label for="c${cnt}-todo_9"></label></td>
-        <td class="${ parseInt(lv) >= 1415 ? 'high' : ''}"><input type="checkbox" id="c${cnt}-todo_10"/><label for="c${cnt}-todo_10"></label></td>
+        <td class="${ parseInt(lv) >= 1415 ? 'high' : ''}"><input type="checkbox" id="c${cnt}-todo_9"/><label for="c${cnt}-todo_9"></label></td>
         <td onclick="del(this)">x</td>
     </tr>`;
 
@@ -139,8 +138,7 @@ function syncStorage() {
                         <td><input type="checkbox" id="c${idx}-todo_6" ${el.todo6 ? 'checked' : '' }/><label for="c${idx}-todo_6"></label></td>
                         <td class="${ parseInt(el.lv) < 1475 ? 'high' : ''}"><input type="checkbox" id="c${idx}-todo_7" ${el.todo7 ? 'checked' : '' }/><label for="c${idx}-todo_7"></label></td>
                         <td class="${ parseInt(el.lv) < 1490 ? 'high' : ''}"><input type="checkbox" id="c${idx}-todo_8" ${el.todo8 ? 'checked' : '' }/><label for="c${idx}-todo_8"></label></td>
-                        <td><input type="checkbox" id="c${idx}-todo_9" ${el.todo9 ? 'checked' : '' }/><label for="c${idx}-todo_9"></label></td>
-                        <td class="${ parseInt(el.lv) >= 1415 ? 'high' : ''}"><input type="checkbox" id="c${idx}-todo_10" ${el.todo10 ? 'checked' : '' }/><label for="c${idx}-todo_10"></label></td>
+                        <td class="${ parseInt(el.lv) >= 1415 ? 'high' : ''}"><input type="checkbox" id="c${idx}-todo_9" ${el.todo9 ? 'checked' : '' }/><label for="c${idx}-todo_9"></label></td>
                         <td onclick="del(this)">x</td>
                     </tr>`;
     
